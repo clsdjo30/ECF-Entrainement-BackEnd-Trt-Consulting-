e352729a38bcfc46ca7ff13f47d0977996711fbe
Cedric Le Sergent <lesergent.cedric@gmail.com>
2022-08-10 20:03:27 +0200

    install fontawesome -

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,7 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+// Installation Fontawesome
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
