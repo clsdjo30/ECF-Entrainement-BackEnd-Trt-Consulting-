e2f7bdd5cd26ff697bae53a20e43f8feda2ab20c
Cedric Le Sergent <lesergent.cedric@gmail.com>
2022-08-11 15:38:56 +0200

    add build path to webpack.config.js - install img-loader & update package.json - add image on home template - complete navbar template

--- assets/app.js
+++ assets/app.js
@@ -11,6 +11,21 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
+
+// Toggler menu
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+});
+
 // Installation Fontawesome
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
