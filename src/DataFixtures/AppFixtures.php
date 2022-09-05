be886d4c616f9a5545ac9422de8013bc5f80d099
Cedric Le Sergent <87324244+clsdjo30@users.noreply.github.com>
2022-09-05 08:02:54 +0200

    Modify Fixture to hash password - add new button en Candidate profil

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,67 +2,81 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\CandidateFactory;
+
+use App\Entity\Candidate;
+use App\Entity\Consultant;
+use App\Entity\User;
 use App\Factory\ConsultantFactory;
-use App\Factory\RecruiterFactory;
 use App\Factory\UserFactory;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
+/**
+ * @codeCoverageIgnore
+ */
 class AppFixtures extends Fixture
 {
+    /**
+     * Encodeur de mot de passe
+     *
+     * @var UserPasswordHasherInterface
+     */
+    private UserPasswordHasherInterface $encoder;
+
+    public function __construct(
+        UserPasswordHasherInterface $encoder,
+
+    ) {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // Création des Utilisateurs de test
-        UserFactory::createOne([
-            'email' => 'admin@trt-consulting.com',
-            'roles' => ['ROLE_ADMIN'],
-            'password' => 'password',
-            'createdAt' => new DateTime(),
-            'updatedAt' => new DateTime(),
-            'isValidated' => true,
-        ]);
 
-        $consultant = UserFactory::createOne([
-            'email' => 'consultant@trt-consulting.com',
-            'roles' => ['ROLE_CONSULTANT'],
-            'password' => 'password',
-            'createdAt' => new DateTime(),
-            'updatedAt' => new DateTime(),
-            'isValidated' => true,
-            'isVerified' => true,
-        ]);
-        ConsultantFactory::createOne([
-            'user' => $consultant,
-        ]);
-        $candidate = UserFactory::createOne([
-            'email' => 'candidat@trt-consulting.com',
-            'roles' => ['ROLE_CANDIDATE'],
-            'password' => 'password',
-            'createdAt' => new DateTime(),
-            'updatedAt' => new DateTime(),
-            'isValidated' => true,
-            'isVerified' => true,
-        ]);
-        CandidateFactory::createOne([
-            'user' => $candidate,
-            'firstname' => 'cedric',
-            'lastname' => 'le sergent',
-            'cvFile' => 'mon Super CV',
-        ]);
-        $recruiter = UserFactory::createOne([
-            'email' => 'recruiter@trt-consulting.com',
-            'roles' => ['ROLE_RECRUITER'],
-            'password' => 'password',
-            'createdAt' => new DateTime(),
-            'updatedAt' => new DateTime(),
-            'isValidated' => true,
-            'isVerified' => true,
-        ]);
-        RecruiterFactory::createOne([
-            'user_id' => $recruiter,
-        ]);
+
+        // Création des Utilisateurs de test
+        // Administrateur
+        $admin = (new User());
+        $admin->setEmail('admin@trt-consulting.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->encoder->hashPassword($admin, 'password'));
+
+        //Consultant
+
+        $userConsultant = new User();
+        $userConsultant->setEmail('consultant@trt-consulting.com');
+        $userConsultant->setRoles(['ROLE_CONSULTANT']);
+        $userConsultant->setPassword($this->encoder->hashPassword($userConsultant, 'password'));
+        $userConsultant->setIsVerified(true);
+        $userConsultant->setIsValidated(true);
+
+        $consultant = (new Consultant())
+            ->setUser($userConsultant);
+
+        // Candidate
+        $userCandidate = new User();
+        $userCandidate->setEmail('candidate@trt-consulting.com');
+        $userCandidate->setRoles(['ROLE_CANDIDATE']);
+        $userCandidate->setPassword($this->encoder->hashPassword($userCandidate, 'password'));
+        $userCandidate->setIsVerified(true);
+        $userCandidate->setIsValidated(true);
+
+        $candidate = (new Candidate())
+            ->setUser($userCandidate);
+
+        // Recruiter
+
+        $userRecruiter = new User();
+        $userRecruiter->setEmail('recruiter@trt-consulting.com');
+        $userRecruiter->setRoles(['ROLE_RECRUITER']);
+        $userRecruiter->setPassword($this->encoder->hashPassword($userRecruiter, 'password'));
+        $userRecruiter->setIsVerified(true);
+        $userRecruiter->setIsValidated(true);
+
+        $recruiter = (new Candidate())
+            ->setUser($userRecruiter);
 
 
         // Création de 5 CONSULTANT
@@ -78,6 +92,14 @@ class AppFixtures extends Fixture
             ConsultantFactory::createOne([
                 'user' => $user,
             ]);
+
+
+            $manager->persist($admin);
+            $manager->persist($consultant);
+            $manager->persist($candidate);
+            $manager->persist($recruiter);
+
+            $manager->flush();
         }
 
     }
