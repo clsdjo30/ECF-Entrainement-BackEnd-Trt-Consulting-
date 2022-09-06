dfdcb7a4d2e7e67e50fabefcb258a8022125ae47
Cedric Le Sergent <87324244+clsdjo30@users.noreply.github.com>
2022-09-06 10:45:15 +0200

    create CandidateType.php - Change Candidate route - Add route to navigation -

--- src/Controller/Admin/CandidateController.php
+++ src/Controller/Admin/CandidateController.php
@@ -4,9 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Announce;
 use App\Repository\AnnounceRepository;
+use App\Repository\CandidateRepository;
 use App\Repository\UserRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,8 +17,24 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/candidate'), isGranted('ROLE_CANDIDATE')]
 class CandidateController extends AbstractController
 {
-    #[Route('/', name: 'app_candidate_home', methods: ('GET'))]
-    public function index(
+    #[Route('/', name: 'app_candidate', methods: ('GET'))]
+    public function index(CandidateRepository $candidateRepository): Response
+    {
+        return $this->render('candidate/index.html.twig', [
+            'candidate' => $candidateRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/details', name: 'app_candidate_details', methods: ('GET'))]
+    #[ParamConverter('candidate', options: ['id' => 'candidate_id'])]
+    public function details(): Response
+    {
+        return $this->render('candidate/profil/details.html.twig');
+    }
+
+    #[Route('/annonces', name: 'app_candidate_announces', methods: ('GET'))]
+    #[ParamConverter('candidate', options: ['id' => 'candidate_id'])]
+    public function showAnnounces(
         UserRepository $userRepository,
         AnnounceRepository $announceRepository,
         PaginatorInterface $paginator,
@@ -35,17 +53,19 @@ class CandidateController extends AbstractController
             6
         );
 
-        return $this->render('candidate/index.html.twig', [
+        return $this->render('candidate/announce/index.html.twig', [
             'announces' => $announces,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_candidate_announce_show', methods: ['GET', 'POST'])]
-    public function showAnnonce(
-        Announce $announce
+    #[Route('/annonce/{id}', name: 'app_candidate_announce_show', methods: ['GET', 'POST'])]
+    public function showAnnounceDetails(
+        Announce $announce,
     ): Response {
-        return $this->render('candidate/show_announce_details.html.twig', [
+        return $this->render('candidate/announce/show_announce_details.html.twig', [
             'announce' => $announce,
         ]);
     }
+
+
 }
