15665f426e358ed4ce27cb829f0dfc012dd7b3b1
Cedric Le Sergent <87324244+clsdjo30@users.noreply.github.com>
2022-09-17 12:32:36 +0200

    add disabled on input in candidate_details

--- src/Entity/Recruiter.php
+++ src/Entity/Recruiter.php
@@ -84,7 +84,7 @@ class Recruiter
 
     public function __toString(): string
     {
-        return $this->getUserId();
+        return $this->company_name;
     }
 
     public function getUserId(): ?User
